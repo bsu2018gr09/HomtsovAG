983802a3e8d9db1e02fdb5ab6b266801ffe9800d
ahomtsov <45938061+ahomtsov@users.noreply.github.com>
2019-04-27 22:13:22 +0300

    Добавил несколько новых методов, исправил ошибки

--- 6-1.cpp
+++ 6-1.cpp
@@ -18,7 +18,7 @@ public:
 	/**
 	 * Конструктор по умолчанию.
 	 */
-	Vector() {} //  "мусор" внутри х,y,z. Тебя это не смущает????
+	Vector() { x = 0; y = 0; z = 0; }
 
 	/**
 	 * Деструктор.
@@ -30,12 +30,12 @@ public:
 
 	/**
 	 * Сложение двух векторов.
-	 * Сложение вектора с числом невозможно.///А почему??????????????
+	 * Сложение вектора с числом невозможно.
 	 * @param Vector left
 	 * @param Vector right
 	 * @return Vector
 	 */
-	friend Vector operator + (Vector& left, Vector& right)  // а почему не член-класса, почему friend??? Обоснуй!!!!
+	friend Vector operator + (const Vector& left, const Vector& right)  
 	{
 		return Vector
 		(
@@ -51,7 +51,7 @@ public:
 	 * @param Vector right
 	 * @return Vector
 	 */
-	friend Vector operator - (Vector& left, Vector& right)
+	friend Vector operator - (const Vector& left, const Vector& right)
 	{
 		return Vector
 		(
@@ -63,12 +63,11 @@ public:
 
 	/**
 	 * Умножение вектора на число.
-	 * Число должно быть записано в переменную, т.к. происходит передача по ссылке ///А зачем??????????????
 	 * @param Vector left
 	 * @param double right
 	 * @return Vector
 	 */
-	friend Vector operator * (Vector& left, double& right)   
+	friend Vector operator * (const Vector& left, const double right)   
 	{
 		return Vector
 		(
@@ -79,30 +78,30 @@ public:
 	}
 
 	/**
+	 * Умножение числа на вектор.
+	 * @param double left
+	 * @param Vector right
+	 * @return Vector
+	 */
+	friend Vector operator * (const double left, const Vector& right)
+	{
+		return Vector
+		(
+			left * right.x, 
+			left * right.y,
+			left * right.z
+		);
+	}
+
+	/**
 	 * Скалярное произведение двух векторов.
 	 * @param Vector left
 	 * @param Vector right
 	 * @return double
 	 */
-	friend double operator * (Vector& left, Vector& right) 
+	friend double operator * (const Vector& left, const Vector& right) 
 	{
 		return left.x * right.x + left.y * right.y + left.z * right.z;
-	}
-
-	/**
-	 * Векторное произведение двух векторов.
-	 * @param Vector left
-	 * @param Vector right
-	 * @return Vector
-	 */
-	friend Vector operator & (Vector& left, Vector& right)   // странный выбор значка ))
-	{
-		return Vector
-		(
-			left.y * right.z - left.z * right.y, 
-			-(left.x * right.z - left.z * right.x), 
-			left.x * right.y - left.y * right.x
-		);
 	}
 
 	/**
@@ -138,6 +137,58 @@ public:
 		}
 		return *this;
 	}
+
+	/**
+	 * Векторное произведение двух векторов.
+	 * @param Vector left
+	 * @param Vector right
+	 * @return Vector
+	 */
+	friend Vector vectorProduct(const Vector& left, const Vector& right)
+	{
+		return Vector
+		(
+			left.y * right.z - left.z * right.y,
+			-(left.x * right.z - left.z * right.x),
+			left.x * right.y - left.y * right.x
+		);
+	}
+
+	/**
+	 * Смешанное произведение трёх векторов.
+	 * @param Vector a
+	 * @param Vector b
+	 * @param Vector c
+	 * @return double
+	 */
+	friend double mixedProduct(const Vector& a, const Vector& b, const Vector& c)
+	{
+		return (Vector(a.y * b.z - a.z * b.y, -(a.x * b.z - a.z * b.x), a.x * b.y - a.y * b.x) * c);
+	}
+
+	/**
+	 * Нахождение длины вектора.
+	 * @param Vector i
+	 * @return double
+	 */
+	friend double vectorLength(const Vector& i)
+	{
+		return sqrt(i.x * i.x + i.y * i.y + i.z * i.z);
+	}
+
+	/**
+	 * Угол между векторами.
+	 * Скалярное произведение векторов = произведение их длин * косинус угла между ними,
+	   отсюда и итоговая ф-ла.
+	 * Угол измеряется в радианах.
+	 * @param Vector left
+	 * @param Vector right
+	 * @return Vector
+	 */
+	friend double vectorAngle(const Vector& left, const Vector& right)
+	{
+		return acos((left * right) / (vectorLength(left) * vectorLength(right)));
+	}
 private:
 	double x;
 	double y;
@@ -154,10 +205,8 @@ int main()
 	Vector b(57, 10, 36);
 	cout << "Значения координат вектора после вызова конструктора с параметрами:\t" << b << endl;
 
-	Vector c(3, 2, 7);
+	Vector c(3, 7, 0);
 	cout << "Значения координат вектора после вызова конструктора с параметрами:\t" << c << endl;
-
-	double x(3.0);
 
 	cout << "Работа оператора сложения:\t"; 
 	cout << b + c;
@@ -167,17 +216,34 @@ int main()
 	cout << b - c;
 	cout << endl;
 
-	cout << "Работа оператора умножения на число:\t";
-	cout << b * x;
+	cout << "Работа оператора умножения вектора на число:\t";
+	cout << b * 3;
+	cout << endl;
+
+	cout << "Работа оператора умножения числа на вектор:\t";
+	cout << 3 * b;
 	cout << endl;
 
 	cout << "Работа оператора скалярного умножения:\t";
 	cout << b * c;
 	cout << endl;
 
-	cout << "Работа оператора векторного умножения и присваивания:\t";
-	a = b & c;
+	cout << "Работа метода векторного умножения и оператора присваивания:\t";
+	a = vectorProduct(b, c);
 	cout << a;
+	cout << endl;
+
+	cout << "Работа метода смешанного умножения:\t";
+	Vector d(1, 1, 1);
+	cout << mixedProduct(b, c, d);
+	cout << endl;
+
+	cout << "Работа метода нахождения длины:\t";
+	cout << vectorLength(c);
+	cout << endl;
+
+	cout << "Работа метода нахождения угла между векторами:\t";
+	cout << vectorAngle(b, c);
 	cout << endl;
 
 	system("pause");
