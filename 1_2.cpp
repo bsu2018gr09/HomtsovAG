2dc5e2b0f2769db496fdfbbe82aed05941ff7e8a
ahomtsov <45938061+ahomtsov@users.noreply.github.com>
2019-05-31 10:37:09 +0300

    Update 1_2.cpp

--- 1_2.cpp
+++ 1_2.cpp
@@ -1,4 +1,4 @@
-﻿/*1.2. Положительные элементы массива А(N) переставить в конец массива, сохраняя порядок следования. 
+/*1.2. Положительные элементы массива А(N) переставить в конец массива, сохраняя порядок следования.
 Отрицательные элементы расположить в порядке убывания. Дополнительный массив не использовать.*/
 #include<iostream>
 #include<time.h>
@@ -7,10 +7,10 @@ using namespace std;
 
 int * pA = nullptr;
 
-int* findMemory(int N) 
+int* findMemory(int N)
 {
 	pA = new(nothrow) int[N];
-	if (!pA) 
+	if (!pA)
 	{
 		cout << "error";
 		return 0;
@@ -18,32 +18,32 @@ int* findMemory(int N)
 	return pA;
 }
 
-void delMemory(int *pA) 
+void delMemory(int *pA)
 {
 	delete[] pA;
 	pA = nullptr;
 }
 
-void printArr(int *arr, int n) 
+void printArr(int *arr, int n)
 {
-	for (int i = 0; i < n; ++i) 
+	for (int i = 0; i < n; ++i)
 	{
 		cout << *(arr + i) << " ";
 	}
 }
 
-void random(int *arr, int n) 
+void random(int *arr, int n)
 {
 	srand(time(0));
 	for (int i = 0; i < n; ++i)
 	{
-		*(arr + i) = rand() % (10 - (-10) + 1) + (-10);//что за 10? откуда???
+		*(arr + i) = rand() % (10 - (-10) + 1) + (-10);
 	}
 }
 
-int main() 
+int main()
 {
-	int N{0}, cnt{0};
+	int N{ 0 }, cnt{ 0 };
 	cout << "Enter array dimension\n";
 	cin >> N;
 
@@ -54,21 +54,28 @@ int main()
 	printArr(pA, N);
 	cout << '\n';
 
-	for (int i = N-1; i >= 0; --i)			//положительные элементы в конец 
-	{    
-		if (*(pA + i) > 0) 
+	for (int i = N - 1; i >= 0; --i)			//положительные элементы в конец 
+	{
+		if (*(pA + i) > 0)
 		{
 			swap(*(pA + i), *(pA + N - 1 - cnt));
 			cnt++;
 		}
 	}
 
-	for (int j = 0; j < N ; ++j)			//отрицательные по убыванию	//Зачем тут до N? В конце же уже положительные!!!			
-	for (int i = 0; i < N ; ++i) 
+	for (int j = 0; j < N - 1; ++j)			//отрицательные по убыванию 
 	{
-		if (*(pA + i) < *(pA + i + 1)) 
-		if (*(pA + i + 1) <= 0)
-			swap(*(pA + i), *(pA + i+ 1));	
+		int flag = 0;
+		for (int i = 0; i < N - j; ++i) 
+		{
+			if (*(pA + i) < *(pA + i + 1))
+				if (*(pA + i + 1) <= 0) 
+				{
+					swap(*(pA + i), *(pA + i + 1));
+					flag++;
+				}
+		}
+		if (!flag) { break; }
 	}
 
 	cout << '\n';
